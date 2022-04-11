deff56ffa98b9f3636322a2643c6fd42a19bde18
TahseenSust <tahseen19@student.sust.edu>
2022-04-12 01:36:02 +0600

    project setup

---
+++ router/inboxRouter.js
@@ -0,0 +1,12 @@
+// external imports
+const express = require("express");
+
+const { getInbox } = require("../controller/inboxController");
+const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+
+const router = express.Router();
+
+// inbox page
+router.get("/", decorateHtmlResponse("Inbox"), getInbox);
+
+module.exports = router;
